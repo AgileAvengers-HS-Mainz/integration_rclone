debaf437dfa1e21b82b4b17db392aeade0c91a8c
Karim Ouhammi <ouhammik@gmail.com>
2023-11-27 19:29:13 +0000

    Update __init__.py

--- custom_components/tasks_service/__init__.py
+++ custom_components/tasks_service/__init__.py
@@ -1,93 +1,52 @@
 DOMAIN = "tasks_service"
 
 
-""" ATTR_ID = "id"
-DEFAULT_ID = "DEFAULT_ID" """
+import requests
+from task import Task
+
 
 ATTR_NAME = "name"
 DEFAULT_NAME = "Standing"
 
-""" ATTR_TYPE = ""
-DEFAULT_TYPE = "DEFAULT_TYPE"
+# Getting the tasks data from the backend using a get request on /tasks Endpoint
+# api-endpoint
+URL = "http://localhost:3000/api/tasks"
 
-ATTR_DESCRIPTION = ""
-DEFAULT_DESCRIPTION = "DEFAULT_DESCRIPTION"
+# sending get request and saving the response as response object
+response = requests.get(url = URL)
 
-ATTR_SOURCE_PATH = ""
-DEFAULT_SOURCE_PATH = "DEFAULT_SOURCE_PATH"
+# extracting data in json format
+tasksData: Task = response.json()
 
-ATTR_SOURCE_TYPE = ""
-DEFAULT_SOURCE_TYPE = "DEFAULT_SOURCE_TYPE"
-
-ATTR_DESTINATION_PATH = ""
-DEFAULT_DESTINATION_PATH = "DEFAULT_DESTINATION_PATH"
-
-ATTR_DESTINATION_TYPE = ""
-DEFAULT_DESTINATION_TYPE = "DEFAULT_DESTINATION_TYPE" """
-
+# Replacing any spaces with underscores in each task name
+for task in tasksData:
+    task["name"] = task["name"].replace(' ', '_')
 
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
 
-    # Creating functions dynamically with names based on the elements of a list.
-    # It's generally not recommended due to security risks associated with
-    # executing arbitrary code. However, I will go for this approach for now.
-    def create_functions_with_exec(elements):
-        for element in elements:
-            # Define the function string
-            function_string = f"def handle_{element}(call):\n    name = call.data.get(ATTR_NAME, DEFAULT_NAME)\n    hass.states.set('tasks_service.{element}', name)"
-            # Execute the function string
-            exec(function_string, globals())
-
-    # Example list of elements
-    my_list = ['apple', 'banana', 'orange']
-
-    # Create functions based on the list elements using exec
-    create_functions_with_exec(my_list)
-
     def handle_hello(call):
         """Handle the service call."""
-        """ id = call.data.get(ATTR_ID, DEFAULT_ID) """
-
         name = call.data.get(ATTR_NAME, DEFAULT_NAME)
 
         hass.states.set("tasks_service.hello", name)
 
-        """type = call.data.get(ATTR_TYPE, DEFAULT_TYPE)
-        description = call.data.get(ATTR_DESCRIPTION, DEFAULT_DESCRIPTION)
-        source_path = call.data.get(ATTR_SOURCE_PATH, DEFAULT_SOURCE_PATH)
-        source_type = call.data.get(ATTR_SOURCE_TYPE, DEFAULT_SOURCE_TYPE)
-        destination_path = call.data.get(ATTR_DESTINATION_PATH, DEFAULT_DESTINATION_PATH)
-        destination_type = call.data.get(ATTR_DESTINATION_TYPE, DEFAULT_DESTINATION_TYPE) """
-
-        name = call.data.get(ATTR_NAME, "done")
-        hass.states.set("tasks_service.hello", name)
-
     hass.services.register(DOMAIN, "hello", handle_hello)
-    for element in my_list:
-        hass.services.register(DOMAIN, element, "handle_" + element)
+    for task in tasksData:
+        hass.services.register(DOMAIN, task["name"], "handle_" + task["name"])
 
     # Return boolean to indicate that initialization was successful.
     return True
 
 
-def create_functions_with_exec(elements):
-    for element in elements:
-        # Define the function string
-        function_string = f"def {element}():\n    print('This is the function for {element}')"
-
+# Creating functions dynamically with names based on the elements of the data list.
+# It's generally not recommended due to security risks associated with
+# executing arbitrary code. However, I will go for this approach for now.
+def create_functions_with_exec(listOfTasks):
+    for task in listOfTasks:
+        # Define the function string using task name
+        function_string = f"def handle_{task['name']}(call):\
+            name = call.data.get(ATTR_NAME, DEFAULT_NAME)\
+            hass.states.set('tasks_service.{task['name']}', name)"
         # Execute the function string
         exec(function_string, globals())
-
-# Example list of elements
-my_list = ['apple', 'banana', 'orange']
-
-# Create functions based on the list elements using exec
-create_functions_with_exec(my_list)
-
-# Now you can call the functions using their corresponding names
-for element in my_list:
-    print(f"Calling function for {element}:")
-    globals()[element]()
-
-    
