3ff94a01a72a4aa2d8dd91a5ec098224d8dbc6bd
Karim Ouhammi <ouhammik@gmail.com>
2024-01-12 08:12:03 +0000

    Add integration_rclone unit tests

---
+++ tests/components/integration_rclone/test_init.py
@@ -0,0 +1,43 @@
+"""Test integration_rclone component setup process."""
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+from homeassistant.config_entries import ConfigEntry
+
+from unittest.mock import patch
+
+from custom_components.integration_rclone import (
+    async_setup_entry,
+    async_unload_entry,
+    async_reload_entry
+)
+
+from custom_components.integration_rclone.const import DOMAIN as DOMAIN_INTEGRATION_RCLONE
+
+async def setup_entry(hass, entry):
+    """Test that setup entry works."""
+    with patch("async_setup", return_value=True):
+        assert await async_setup_entry(hass, entry) is True
+
+async def test_async_setup(hass):
+    """Test the component gets setup."""
+    assert await async_setup_component(hass, DOMAIN_INTEGRATION_RCLONE, {}) is True
+
+async def test_setup_entry_successful(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> None:
+    """Test setup entry is successful."""
+    config_entry = await async_setup_entry(hass, entry)
+
+    assert hass.data[DOMAIN_INTEGRATION_RCLONE]
+    assert config_entry.entry_id in hass.data[DOMAIN_INTEGRATION_RCLONE]
+    assert hass.data[DOMAIN_INTEGRATION_RCLONE][config_entry.entry_id].master
+
+async def test_unload_entry(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> None:
+    """Test being able to unload an entry."""
+    config_entry = await async_setup_entry(hass, entry)
+    assert hass.data[DOMAIN_INTEGRATION_RCLONE]
+
+    assert await async_unload_entry(hass, config_entry)
+    assert not hass.data[DOMAIN_INTEGRATION_RCLONE]
