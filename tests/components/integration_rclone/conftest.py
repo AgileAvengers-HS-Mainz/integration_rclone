fc3fbf669f69489fb96ae7e6dd343ea29c613cbb
Karim Ouhammi <ouhammik@gmail.com>
2024-01-12 09:00:21 +0000

    Update conftest.py

--- tests/components/integration_rclone/conftest.py
+++ tests/components/integration_rclone/conftest.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
-    """Enable custom components"""
+    """Enable custom components."""
     yield
